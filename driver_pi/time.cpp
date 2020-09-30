84815b50c9c312409da66f43f222f452990652cb
Juri Bieler <juribieler@gmail.com>
2020-09-30 13:38:30 +0200

    make get_time_system_ms return time since programme start

--- driver_pi/time.cpp
+++ driver_pi/time.cpp
@@ -4,24 +4,37 @@
 
 using namespace std::chrono;
 
+uint64_t start_time_offset_us = 0;
+uint64_t start_time_offset_ms = 0;
+
+void init_time() {
+  start_time_offset_ms =
+      std::chrono::duration_cast<std::chrono::milliseconds>(
+          std::chrono::system_clock::now().time_since_epoch())
+          .count();
+  start_time_offset_us =
+      std::chrono::duration_cast<std::chrono::microseconds>(
+          std::chrono::system_clock::now().time_since_epoch())
+          .count();
+}
+
 uint32_t get_time_system_ms() {
-  // get time in us, convert to ms, wrap one week
-  // struct timeval time;
-  // gettimeofday(&time, NULL);
-  // return time.tv_msec;
-  // return (uint32_t)(get_time_system_us() / 1000);
-  return (uint32_t)std::chrono::duration_cast<std::chrono::milliseconds>(
-             std::chrono::system_clock::now().time_since_epoch())
-      .count();
+  if (start_time_offset_ms == 0) {
+    init_time();
+  }
+  return (uint32_t)(std::chrono::duration_cast<std::chrono::milliseconds>(
+                        std::chrono::system_clock::now().time_since_epoch())
+                        .count() -
+                    start_time_offset_ms);
 }
 
 uint64_t get_time_system_us() {
-  // get time in us
-  // struct timeval time;
-  // gettimeofday(&time, NULL);
-  // return time.tv_usec;
+  if (start_time_offset_us == 0) {
+    init_time();
+  }
   return std::chrono::duration_cast<std::chrono::microseconds>(
              std::chrono::system_clock::now().time_since_epoch())
-      .count();
+             .count() -
+         start_time_offset_us;
 }
 #endif
