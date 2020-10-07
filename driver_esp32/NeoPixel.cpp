dc0f46c415c51e8e5571d8796c5ab6f1f2fc51be
michaelrenoo <michaelreno19@gmail.com>
2020-10-07 16:33:22 +0200

    Added copyright and inline function to neopixel

--- driver_esp32/NeoPixel.cpp
+++ driver_esp32/NeoPixel.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file neoPixel.cpp
+ * @author  Juri Bieler, Michael Reno (michaelreno19@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2020-10-06
+ *
+ * @copyright Copyright (c) 2020
+ * with some codes from https://github.com/FozzTexx/ws2812-demo
+ * Created 19 Nov 2016 by Chris Osborn <fozztexx@fozztexx.com>
+ * http://insentricity.com
+ * 
+ */
 
 #include "NeoPixel.hpp"
 
