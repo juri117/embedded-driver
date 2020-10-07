863e728ceee5a6498cbffbaf2cb8f93c6ab21d5a
juri117 <juribieler@gmail.com>
2020-10-07 20:55:26 +0200

    add dummy method for pi neoPixel

--- driver_pi/NeoPixel.cpp
+++ driver_pi/NeoPixel.cpp
@@ -3,6 +3,11 @@
 
 #if BUILD_TARGET == TARGET_PI3
 
-static const char* TAG = "neoPixel";
+// static const char* TAG = "neoPixel";
+
+NeoPixel::NeoPixel() {}
+void NeoPixel::init(gpio_num_t gpio, uint16_t neo_count) {}
+void NeoPixel::set_color(uint16_t index, uint8_t r, uint8_t g, uint8_t b) {}
+void NeoPixel::set_brightness(uint16_t index, float brightness) {}
 
 #endif
