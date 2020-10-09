0dbb863a9898ade5cad1cbd6690a5f4957fdad70
michaelrenoo <michaelreno19@gmail.com>
2020-10-09 14:34:47 +0200

    Added rainbow function

--- driver_esp32/NeoPixel.cpp
+++ driver_esp32/NeoPixel.cpp
@@ -208,7 +208,69 @@ void NeoPixel::set_brightness(uint16_t index, float brightness) {
 }
 
 void NeiPixel::rainbow() {
+  const uint8_t anim_step = 10;
+  const uint8_t anim_max = 250;
+  const uint8_t pixel_count = PIXEL_COUNT; // Number of your "pixels"
+  const uint8_t delay = 10; // duration between color changes
+  rgbVal color = makeRGBVal(anim_max, 0, anim_max);
+  uint8_t step = 0;
+  rgbVal color2 = makeRGBVal(anim_max, 0, 0);
+  uint8_t step2 = 0;
+  rgbVal *pixels;
 
+
+  pixels = malloc(sizeof(rgbVal) * pixel_count);
+
+  while (1) {
+    color = color2;
+    step = step2;
+
+    for (uint8_t i = 0; i < pixel_count; i++) {
+      pixels[i] = color;
+
+      if (i == 1) {
+        color2 = color;
+        step2 = step;
+      }
+
+      switch (step) {
+      case 0:
+        color.g += anim_step;
+        if (color.g >= anim_max)
+          step++;
+        break;
+      case 1:
+        color.r -= anim_step;
+        if (color.r == 0)
+          step++;
+        break;
+      case 2:
+        color.b += anim_step;
+        if (color.b >= anim_max)
+          step++;
+        break;
+      case 3:
+        color.g -= anim_step;
+        if (color.g == 0)
+          step++;
+        break;
+      case 4:
+        color.r += anim_step;
+        if (color.r >= anim_max)
+          step++;
+        break;
+      case 5:
+        color.b -= anim_step;
+        if (color.b == 0)
+          step = 0;
+        break;
+      }
+    }
+
+    set_color(pixel_count, color.r, color.g, color.b);
+
+    delay_ms(delay);
+  }
 }
 
 #endif
